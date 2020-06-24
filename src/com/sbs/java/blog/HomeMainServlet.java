b75c1a61fde423c1551c966b7b9395ee98274356
duerotkd <dureotkd123@naver.com>
2020-06-24 22:49:45 +0900

    자바 게시판 생성

---
+++ src/com/sbs/java/blog/HomeMainServlet.java
@@ -0,0 +1,27 @@
+package com.sbs.java.blog;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/home/main") // 브라우저  입력창에 실행되는 놈 !
+
+public class HomeMainServlet extends HttpServlet {
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
